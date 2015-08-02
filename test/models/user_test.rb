978483108d086ebd4a946ce4ec72ef2ba1c2f5bf
fuczak <prus.damian@gmail.com>
2015-08-02 19:25:02 +0000

    Add user email downcasing callback test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -52,6 +52,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
   
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+  
   test "password should be present" do
     @user.password = "    "
     assert_not @user.valid?
