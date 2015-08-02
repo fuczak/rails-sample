b1303b834b55879b28f26655ae235b8894f4c44b
fuczak <prus.damian@gmail.com>
2015-08-02 19:26:48 +0000

    Add stronger valid email regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-    before_save { self.email = email.downcase } #should be self.email = self.email.downcase
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    before_save { self.email.downcase! } #should be self.email = self.email.downcase
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :name, presence: true, length: { maximum: 50 }
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
     validates :password, presence: true, length: { minimum: 6 }
