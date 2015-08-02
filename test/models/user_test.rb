de7caa21be8f2132126bd5a767a239d3da3bbc8c
fuczak <prus.damian@gmail.com>
2015-08-02 18:47:18 +0000

    Add email format validation, fix test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -31,7 +31,7 @@ class UserTest < ActiveSupport::TestCase
   
   test "email validation should accept valid addresses" do
     valid_addresses = %w[user@example.com USER@foo.com A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
-    valid_addresses each do |valid_address|
+    valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid."
     end
@@ -39,9 +39,9 @@ class UserTest < ActiveSupport::TestCase
   
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
-    invalid_addresses each do |invalid_address|
+    invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
-      assert_not user.valid? "#{invalid_address} should not be valid."
+      assert_not @user.valid? "#{invalid_address} should not be valid."
     end
   end
 end
